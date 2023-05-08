dfed20a033d81cde41500cdcf5e4590f0b551aa0
MbanduVincent <mbanduvincentochango@gmail.com>
2023-05-08 17:30:33 +0300

    commit

---
+++ practical2/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          fontFamily: 'Lato',
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+              .copyWith(secondary: Colors.deepOrange),
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => const ProductDetailScreen(),
+        });
+  }
+}
